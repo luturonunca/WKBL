6521734157373eca92daa9bd24b1b464357cb9bc
luturonunca <nunezcas@iap.fr>
2026-05-09 14:48:32 +0200

    Add vkick1, t_sn2, vkick2, t_merge to BNS component
    Fields written by output_part.f90 via generic_dump. Kicks converted
    to km/s; times kept in code units. All fall back to zeros if absent.

--- wkbl/astro/_bns.py
+++ wkbl/astro/_bns.py
@@ -47,11 +47,43 @@ class _bns(comp.Component):
         except KeyError:
             self.Eu = np.zeros(_n)
 
+        try:
+            self.vkick1 = comp._yt_get_field(
+                self._ad, [("bns", "particle_vkick1")]
+            ).to("km/s").value
+        except KeyError:
+            self.vkick1 = np.zeros(_n)
+
+        try:
+            self.t_sn2 = np.array(
+                comp._yt_get_field(self._ad, [("bns", "particle_t_sn2")]).value
+            )
+        except KeyError:
+            self.t_sn2 = np.zeros(_n)
+
+        try:
+            self.vkick2 = comp._yt_get_field(
+                self._ad, [("bns", "particle_vkick2")]
+            ).to("km/s").value
+        except KeyError:
+            self.vkick2 = np.zeros(_n)
+
+        try:
+            self.t_merge = np.array(
+                comp._yt_get_field(self._ad, [("bns", "particle_t_merge")]).value
+            )
+        except KeyError:
+            self.t_merge = np.zeros(_n)
+
     def halo_Only(self, center, n, r200, simple=False):
         super().halo_Only(center, n, r200, simple=simple)
         in_halo = np.where(self.r <= n * r200)
-        self.stage = self.stage[in_halo]
-        self.age   = self.age[in_halo]
-        self.metal = self.metal[in_halo]
-        self.Eu    = self.Eu[in_halo]
-        self.r     = self.r[in_halo]
+        self.stage   = self.stage[in_halo]
+        self.age     = self.age[in_halo]
+        self.metal   = self.metal[in_halo]
+        self.Eu      = self.Eu[in_halo]
+        self.vkick1  = self.vkick1[in_halo]
+        self.t_sn2   = self.t_sn2[in_halo]
+        self.vkick2  = self.vkick2[in_halo]
+        self.t_merge = self.t_merge[in_halo]
+        self.r       = self.r[in_halo]
